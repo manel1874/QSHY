161710e99c32ce26e6f4cad5e0ec8aab4de38944
Manuel Santos <33625071+manel1874@users.noreply.github.com>
2021-07-07 14:37:13 +0100

    Update install_cq_libscapi_mpcbenchmark.sh

--- install_cq_libscapi_mpcbenchmark.sh
+++ install_cq_libscapi_mpcbenchmark.sh
@@ -19,7 +19,7 @@ cd
 git clone https://github.com/manel1874/MPC-Benchmark
 mv MPC-Benchmark qMPC-Benchmark
 cd qMPC-Benchmark
-git checkout dev
+git checkout qdev
 cd SemiHonestYao
 cmake . && make
 
